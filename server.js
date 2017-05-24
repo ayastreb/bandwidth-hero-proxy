b70d947139ffbc75de66f8dc2e0c3155a804d30b
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-05-24 19:00:25 +0900

    Remove console.log

--- server.js
+++ server.js
@@ -17,25 +17,21 @@ app.get('/', (req, res) => {
   const imageUrl = req.query.url
   if (!imageUrl.match(/^https?:/i)) return res.status(400).end()
 
-  let originalFileSize = 0
+  let originalSize = 0
   const transformer = sharp().grayscale().toFormat('webp', { quality: QUALITY })
   transformer.on('error', err => console.log(`Error in ${imageUrl}: ${err}`))
   transformer.on('data', processedImage => {
-    console.log(`Compressed: ${imageUrl}`)
     res.writeHead(200, {
       'Content-Type': 'image/webp',
       'Content-Length': processedImage.length,
-      'X-Original-Size': originalFileSize,
-      'X-Bytes-Saved': originalFileSize - processedImage.length
+      'X-Original-Size': originalSize,
+      'X-Bytes-Saved': originalSize - processedImage.length
     })
   })
 
   request
     .get(imageUrl)
-    .on(
-      'response',
-      response => (originalFileSize = response.headers['content-length'])
-    )
+    .on('response', res => (originalSize = res.headers['content-length']))
     .pipe(transformer)
     .pipe(res)
 })
