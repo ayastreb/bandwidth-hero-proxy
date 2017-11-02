462a13c0fb43921dfd1b269ea10af4350c4bd849
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-11-02 17:06:27 +0900

    Fix cookie send

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
           path: imageUrl.path,
           headers: {
             'User-Agent': USER_AGENT,
-            Cookie: req.headers.cookie,
+            Cookie: req.headers.cookie || '',
             'X-Forwarded-For': req.ip
           }
         },
