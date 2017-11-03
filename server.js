9bb5e884ddbab55aa4394050f67cf7f90c3d2b20
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-11-03 19:16:48 +0900

    Refactor proxy

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const zlib = require('zlib')
 
 const PORT = process.env.PORT
 const DEFAULT_QUALITY = 40
-const DEFAULT_TIMEOUT = 5000
+const DEFAULT_TIMEOUT = 3000
 const MIN_COMPRESS_LENGTH = 512
 const USER_AGENT = 'Bandwidth-Hero Compressor'
 
@@ -24,31 +24,35 @@ app.get('/', (req, res) => {
   if (Array.isArray(queryUrl)) queryUrl = queryUrl.join('&url=')
   if (!queryUrl) return res.end('https://github.com/ayastreb/bandwidth-hero-proxy')
   const imageUrl = url.parse(queryUrl)
-  if (imageUrl.protocol !== 'https:' && imageUrl.protocol !== 'http:') return res.status(400).end()
+  if (imageUrl.protocol !== 'https:' && imageUrl.protocol !== 'http:') return terminate()
 
   const protocol = imageUrl.protocol === 'https:' ? https : http
-  protocol
-    .get(
-      {
-        protocol: imageUrl.protocol,
-        host: imageUrl.host,
-        path: imageUrl.path,
-        headers: {
-          Cookie: req.headers.cookie || '',
-          'User-Agent': USER_AGENT,
-          'X-Forwarded-For': req.ip
-        },
-        timeout: DEFAULT_TIMEOUT
-      },
-      proxied => {
-        proxied
-          .pipe(decodeTransformer(req, proxied.headers['content-encoding']))
-          .pipe(imageTransformer(req, res, proxied))
-          .pipe(res)
-          .on('error', err => res.status(400).end())
+  const proxyReq = protocol.get(
+    {
+      protocol: imageUrl.protocol,
+      host: imageUrl.host,
+      path: imageUrl.path,
+      headers: {
+        Cookie: req.headers.cookie || '',
+        'User-Agent': USER_AGENT,
+        'X-Forwarded-For': req.ip
       }
-    )
-    .on('error', err => res.status(400).end())
+    },
+    proxied => {
+      proxied
+        .pipe(decodeTransformer(req, proxied.headers['content-encoding']))
+        .pipe(imageTransformer(req, res, proxied))
+        .pipe(res)
+        .on('error', terminate)
+    }
+  )
+
+  proxyReq.on('error', terminate)
+  proxyReq.setTimeout(DEFAULT_TIMEOUT, terminate)
+
+  function terminate() {
+    return res.status(400).end()
+  }
 })
 
 function decodeTransformer(req, encoding) {
@@ -62,24 +66,24 @@ function decodeTransformer(req, encoding) {
   }
 }
 
-function imageTransformer(req, res, proxied) {
-  const originSize = proxied.headers['content-length']
-  if (proxied.statusCode !== 200 || originSize < MIN_COMPRESS_LENGTH) return new PassThrough()
+function imageTransformer(req, res, origin) {
+  const originSize = origin.headers['content-length']
+  if (origin.statusCode !== 200 || originSize < MIN_COMPRESS_LENGTH) return new PassThrough()
 
-  const jpegOnly = !!req.query.jpeg
+  const format = !!req.query.jpeg ? 'jpeg' : 'webp'
   const isGrayscale = req.query.bw != 0
   const quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY
 
   const transformer = Sharp()
     .grayscale(isGrayscale)
-    .toFormat(jpegOnly ? 'jpeg' : 'webp', { quality })
+    .toFormat(format, { quality })
 
   transformer.on('error', () => res.status(400).end())
   transformer.on('info', info => {
     if (!info) return
 
-    let headers = Object.assign({}, proxied.headers, {
-      'Content-Type': jpegOnly ? 'image/jpeg' : 'image/webp',
+    let headers = Object.assign({}, origin.headers, {
+      'Content-Type': `image/${format}`,
       'Content-Length': info.size,
       'Content-Encoding': 'identity'
     })
