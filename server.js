b6556481021969a0bf99b3b7070f16f7e08e65e1
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-11-03 12:32:08 +0900

    Refactor proxy

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ Raven.config(process.env.SENTRY_DSN).install()
 const app = Express()
 app.use(Raven.requestHandler())
 app.get('/', (req, res) => {
-  const imageUrl = getQueryUrl()
+  let imageUrl = req.query.url
+  if (Array.isArray(imageUrl)) imageUrl = imageUrl.join('&url=')
   if (!imageUrl) return res.end('https://github.com/ayastreb/bandwidth-hero-proxy')
   if (!imageUrl.match(/^https?:/i)) return res.status(400).end()
 
@@ -30,12 +31,37 @@ app.get('/', (req, res) => {
     responseType: 'stream'
   })
     .then(proxied => {
-      if (proxied.headers['content-length'] < MIN_COMPRESS_LENGTH) {
+      const originLength = proxied.headers['content-length']
+      if (originLength < MIN_COMPRESS_LENGTH) {
         return proxied.data.pipe(res).on('error', () => res.status(400).end())
       }
 
+      const jpegOnly = !!req.query.jpeg
+      const isGrayscale = req.query.bw != 0
+      const quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY
+
+      const transformer = Sharp()
+        .grayscale(isGrayscale)
+        .toFormat(jpegOnly ? 'jpeg' : 'webp', { quality })
+
+      transformer.on('error', err => res.status(400).end())
+      transformer.on('info', info => {
+        if (!info) return
+
+        let headers = Object.assign({}, proxied.headers, {
+          'Content-Type': jpegOnly ? 'image/jpeg' : 'image/webp',
+          'Content-Length': info.size
+        })
+
+        if (originLength > 0) {
+          headers['X-Original-Size'] = originLength
+          headers['X-Bytes-Saved'] = originLength - info.size
+        }
+        res.writeHead(200, headers)
+      })
+
       proxied.data
-        .pipe(getTransformer(proxied.headers))
+        .pipe(transformer)
         .pipe(res)
         .on('error', () => res.status(400).end())
     })
@@ -43,41 +69,6 @@ app.get('/', (req, res) => {
       Raven.captureException(err)
       res.status(400).end()
     })
-
-  function getQueryUrl() {
-    let url = req.query.url
-    if (Array.isArray(url)) url = url.join('&url=')
-
-    return url
-  }
-
-  function getTransformer(originHeaders) {
-    const jpegOnly = !!req.query.jpeg
-    const isGrayscale = req.query.bw != 0
-    const quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY
-
-    const transformer = Sharp()
-      .grayscale(isGrayscale)
-      .toFormat(jpegOnly ? 'jpeg' : 'webp', { quality })
-
-    transformer.on('error', err => res.status(400).end())
-    transformer.on('info', info => {
-      if (!info) return
-
-      let headers = Object.assign({}, originHeaders, {
-        'Content-Type': jpegOnly ? 'image/jpeg' : 'image/webp',
-        'Content-Length': info.size
-      })
-
-      if (originHeaders['content-length'] > 0) {
-        headers['X-Original-Size'] = originHeaders['content-length']
-        headers['X-Bytes-Saved'] = originHeaders['content-length'] - info.size
-      }
-      res.writeHead(200, headers)
-    })
-
-    return transformer
-  }
 })
 
 app.use(Raven.errorHandler())
