5f4767ceec180b7bca5703300a733a60fcd5c937
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-11-03 15:31:38 +0900

    Refactor proxy

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 'use strict'
 require('newrelic')
-const Axios = require('axios')
 const Express = require('express')
 const Raven = require('raven')
 const Sharp = require('sharp')
+const PassThrough = require('stream').PassThrough
+const url = require('url')
+const http = require('http')
+const https = require('https')
+const zlib = require('zlib')
 
 const PORT = process.env.PORT
 const DEFAULT_QUALITY = 40
@@ -16,60 +20,79 @@ Raven.config(process.env.SENTRY_DSN).install()
 const app = Express()
 app.use(Raven.requestHandler())
 app.get('/', (req, res) => {
-  let imageUrl = req.query.url
-  if (Array.isArray(imageUrl)) imageUrl = imageUrl.join('&url=')
-  if (!imageUrl) return res.end('https://github.com/ayastreb/bandwidth-hero-proxy')
-  if (!imageUrl.match(/^https?:/i)) return res.status(400).end()
+  let queryUrl = req.query.url
+  if (Array.isArray(queryUrl)) queryUrl = queryUrl.join('&url=')
+  if (!queryUrl) return res.end('https://github.com/ayastreb/bandwidth-hero-proxy')
+  const imageUrl = url.parse(queryUrl)
+  if (imageUrl.protocol !== 'https:' && imageUrl.protocol !== 'http:') return res.status(400).end()
 
-  Axios.get(imageUrl, {
-    headers: {
-      Cookie: req.headers.cookie || '',
-      'User-Agent': USER_AGENT,
-      'X-Forwarded-For': req.ip
-    },
-    timeout: DEFAULT_TIMEOUT,
-    responseType: 'stream'
-  })
-    .then(proxied => {
-      const originLength = proxied.headers['content-length']
-      if (originLength < MIN_COMPRESS_LENGTH) {
-        return proxied.data.pipe(res).on('error', () => res.status(400).end())
+  const protocol = imageUrl.protocol === 'https:' ? https : http
+  protocol
+    .get(
+      {
+        protocol: imageUrl.protocol,
+        host: imageUrl.host,
+        path: imageUrl.path,
+        headers: {
+          Cookie: req.headers.cookie || '',
+          'User-Agent': USER_AGENT,
+          'X-Forwarded-For': req.ip
+        },
+        timeout: DEFAULT_TIMEOUT
+      },
+      proxied => {
+        proxied
+          .pipe(decodeTransformer(req, proxied.headers['content-encoding']))
+          .pipe(imageTransformer(req, res, proxied))
+          .pipe(res)
+          .on('error', err => res.status(400).end())
       }
-
-      const jpegOnly = !!req.query.jpeg
-      const isGrayscale = req.query.bw != 0
-      const quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY
-
-      const transformer = Sharp()
-        .grayscale(isGrayscale)
-        .toFormat(jpegOnly ? 'jpeg' : 'webp', { quality })
-
-      transformer.on('error', err => res.status(400).end())
-      transformer.on('info', info => {
-        if (!info) return
-
-        let headers = Object.assign({}, proxied.headers, {
-          'Content-Type': jpegOnly ? 'image/jpeg' : 'image/webp',
-          'Content-Length': info.size
-        })
-
-        if (originLength > 0) {
-          headers['X-Original-Size'] = originLength
-          headers['X-Bytes-Saved'] = originLength - info.size
-        }
-        res.writeHead(200, headers)
-      })
-
-      proxied.data
-        .pipe(transformer)
-        .pipe(res)
-        .on('error', () => res.status(400).end())
-    })
-    .catch(err => {
-      Raven.captureException(err)
-      res.status(400).end()
-    })
+    )
+    .on('error', err => res.status(400).end())
 })
+
+function decodeTransformer(req, encoding) {
+  switch (encoding) {
+    case 'gzip':
+      return zlib.createGunzip()
+    case 'deflate':
+      return zlib.createDeflate()
+    default:
+      return new PassThrough()
+  }
+}
+
+function imageTransformer(req, res, proxied) {
+  const originSize = proxied.headers['content-length']
+  if (proxied.statusCode !== 200 || originSize < MIN_COMPRESS_LENGTH) return new PassThrough()
+
+  const jpegOnly = !!req.query.jpeg
+  const isGrayscale = req.query.bw != 0
+  const quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY
+
+  const transformer = Sharp()
+    .grayscale(isGrayscale)
+    .toFormat(jpegOnly ? 'jpeg' : 'webp', { quality })
+
+  transformer.on('error', () => res.status(400).end())
+  transformer.on('info', info => {
+    if (!info) return
+
+    let headers = Object.assign({}, proxied.headers, {
+      'Content-Type': jpegOnly ? 'image/jpeg' : 'image/webp',
+      'Content-Length': info.size,
+      'Content-Encoding': 'identity'
+    })
+
+    if (originSize > 0) {
+      headers['X-Original-Size'] = originSize
+      headers['X-Bytes-Saved'] = originSize - info.size
+    }
+    res.writeHead(200, headers)
+  })
+
+  return transformer
+}
 
 app.use(Raven.errorHandler())
 if (PORT > 0) app.listen(PORT, () => console.log(`Listening on ${PORT}`))
