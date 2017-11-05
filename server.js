6c009a62367ebf22a5c93504f06f2852c65b52fd
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-11-05 17:37:42 +0900

    Enable opbeat

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 'use strict'
+const opbeat = require('opbeat').start({
+  appId: process.env.OPBEAT_APP_ID,
+  organizationId: process.env.OPBEAT_ORG_ID,
+  secretToken: process.env.OPBEAT_TOKEN
+})
 const Express = require('express')
 const Raven = require('raven')
 const Sharp = require('sharp')
@@ -109,6 +114,7 @@ app.get('/', (req, res) => {
 })
 
 app.use(Raven.errorHandler())
+app.use(opbeat.middleware.express())
 if (PORT > 0) app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 module.exports = app
