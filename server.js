89118d93cf4f5ce785872b787a4a84b9dcfd0c78
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-11-19 11:52:46 -0600

    Add favicon

--- server.js
+++ server.js
@@ -17,5 +17,6 @@ const PORT = process.env.PORT || 8080
 
 app.enable('trust proxy')
 app.get('/', authenticate, params, proxy)
+app.get('/favicon.ico', (req, res) => res.status(204).end())
 app.use(opbeat.middleware.express())
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
