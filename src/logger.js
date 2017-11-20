5178b67c38044ceff4dfc4c57d82adb5c5efe2db
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-11-19 21:07:07 -0600

    Add origin type

--- src/logger.js
+++ src/logger.js
@@ -1,15 +1,16 @@
 const logger = require('logzio-nodejs').createLogger({
   token: process.env.LOGZIO_TOKEN,
-  host: 'listener.logz.io',
-  type: 'request'
+  host: 'listener.logz.io'
 })
 
-module.exports = (action, req) => {
+module.exports = (type, req) => {
   logger.log({
     message: req.params.url,
-    action,
+    type,
     webp: req.params.webp,
     ip: req.ip,
+    origin_type: req.params.originType,
+    origin_size: req.params.originSize,
     agent: req.headers['user-agent'],
     ...req.log
   })
