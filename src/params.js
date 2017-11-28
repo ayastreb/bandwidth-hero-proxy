601fb202121b2804c740e96dfe94f826f51fd3cc
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-11-28 10:07:51 -0800

    Remove url parse

--- src/params.js
+++ src/params.js
@@ -1,19 +1,14 @@
-const { URL } = require('url')
 const DEFAULT_QUALITY = 40
 
 function params(req, res, next) {
-  let imageUrl = req.query.url
-  if (Array.isArray(imageUrl)) imageUrl = imageUrl.join('&url=')
-  if (!imageUrl) {
+  let url = req.query.url
+  if (Array.isArray(url)) url = url.join('&url=')
+  if (!url) {
     res.setHeader('Location', 'https://bandwidth-hero.com')
     return res.status(302).end()
   }
-  imageUrl = imageUrl.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://')
-  const parsed = new URL(imageUrl)
-  req.params.url = imageUrl
-  req.params.proto = parsed.protocol.replace(':', '')
-  req.params.port = parsed.port
-  req.params.host = parsed.host
+  url = url.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://')
+  req.params.url = url
   req.params.webp = !req.query.jpeg
   req.params.grayscale = req.query.bw != 0
   req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY
--- src/proxy.js
+++ src/proxy.js
@@ -15,9 +15,6 @@ function proxy(req, res) {
         ...pick(req.headers, ['cookie', 'dnt', 'referer']),
         'user-agent': 'Bandwidth-Hero Compressor',
         'x-forwarded-for': req.headers['x-forwarded-for'] || req.ip,
-        'x-forwarded-proto': req.params.proto,
-        'x-forwarded-host': req.params.host,
-        'x-forwarded-port': req.params.port,
         via: '1.1 bandwidth-hero'
       },
       timeout: 10000,
