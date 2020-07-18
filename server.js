f4cfaed9d216f3a9136f826572c5fc7c65222a2f
milahu <milahu@gmail.com>
2020-07-18 21:57:27 +0200

    catch all routes for new URL format

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ const proxy = require('./src/proxy')
 const PORT = process.env.PORT || 8080
 
 app.enable('trust proxy')
-app.get('/', authenticate, params, proxy)
 app.get('/favicon.ico', (req, res) => res.status(204).end())
+app.get('*', authenticate, params, proxy)
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
