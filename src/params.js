c105f7d216b9f0d47c38c97b8e86dc924e8ce587
milahu <milahu@gmail.com>
2020-07-21 12:39:29 +0200

    parse internal query string before protocol

--- src/params.js
+++ src/params.js
@@ -14,16 +14,34 @@ function params(req, res, next) {
     req.params.quality = parseInt(req.query.l, 10) || DEFAULT_QUALITY
   }
   else {
-    // new URL format /https/host/path/file.jpg.c50.webp?queryString
-    const match = req.url.match(/^\/(\w+)\/(.*?).([bc])(\d{1,3})\.(\w+)(\?.*)?$/);
+    // new URL format /k=v&k2=v2/https/host/path/file.jpg.c50.webp?queryString
+    const match = req.url.match(/^(?:\/([^/]+?=[^/]+))?\/(\w+)\/(.*?).([bc])(\d{1,3})\.(\w+)(\?.*)?$/);
     if (match) {
-      req.params.url = match[1] + "://" + match[2]; // protocol + host + path
-      if(match[6]) {
-        req.params.url += match[6]; // query string
+      
+      let queryObj = {};
+      if (match[1]) {
+        // parse internal query string before protocol
+        let input = match[1];
+        const re = /([^?&=]+)(?:=([^&]+))?/g;
+        let match2;
+        while (match2 = re.exec(input)) {
+          queryObj[match2[1]] = decodeURIComponent(match2[2]);
+        }
       }
-      req.params.grayscale = (match[3] == "b");
-      req.params.quality = parseInt(match[4], 10) || DEFAULT_QUALITY;
-      req.params.webp = (match[5] == "webp");
+
+      // reconstruct original URL
+      req.params.url = match[2] + "://" + match[3]; // protocol + host + path
+      if (queryObj.appendPath) {
+        req.params.url += queryObj.appendPath;
+      }
+      if(match[7]) {
+        req.params.url += match[7]; // query string
+      }
+      
+      // set compression parameters
+      req.params.grayscale = (match[4] == "b");
+      req.params.quality = parseInt(match[5], 10) || DEFAULT_QUALITY;
+      req.params.webp = (match[6] == "webp");
     }
   }
   next()
